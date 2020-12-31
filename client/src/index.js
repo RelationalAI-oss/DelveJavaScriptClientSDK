7b1e3c660c9040f7e29799b26ac590fdd377b967
Nate Nystrom <nate.nystrom@gmail.com>
2020-12-31 16:12:05 +0100

    Remove FrontProblem from ClientSDK protocol (#3548)
    Remove FrontProblem from protocol

--- client/src/index.js
+++ client/src/index.js
@@ -19,7 +19,6 @@ import Action from './model/Action';
 import ActionResult from './model/ActionResult';
 import Appl from './model/Appl';
 import Area from './model/Area';
-import ArityMismatchError from './model/ArityMismatchError';
 import AzureIntegration from './model/AzureIntegration';
 import CSVFileSchema from './model/CSVFileSchema';
 import CSVFileSyntax from './model/CSVFileSyntax';
@@ -28,18 +27,15 @@ import CardinalityActionResult from './model/CardinalityActionResult';
 import ClientProblem from './model/ClientProblem';
 import CollectProblemsAction from './model/CollectProblemsAction';
 import CollectProblemsActionResult from './model/CollectProblemsActionResult';
-import ComparisonChainError from './model/ComparisonChainError';
 import Cons from './model/Cons';
 import DefaultIntegration from './model/DefaultIntegration';
 import ExceptionProblem from './model/ExceptionProblem';
 import FileSchema from './model/FileSchema';
 import FileSyntax from './model/FileSyntax';
-import FrontProblem from './model/FrontProblem';
 import ICViolation from './model/ICViolation';
 import ImportAction from './model/ImportAction';
 import ImportActionResult from './model/ImportActionResult';
 import InfraError from './model/InfraError';
-import InlineInconsistent from './model/InlineInconsistent';
 import InstallAction from './model/InstallAction';
 import InstallActionResult from './model/InstallActionResult';
 import Integration from './model/Integration';
@@ -76,7 +72,6 @@ import Relation from './model/Relation';
 import S3Integration from './model/S3Integration';
 import SetOptionsAction from './model/SetOptionsAction';
 import SetOptionsActionResult from './model/SetOptionsActionResult';
-import ShadowError from './model/ShadowError';
 import Source from './model/Source';
 import StatusAction from './model/StatusAction';
 import StatusActionResult from './model/StatusActionResult';
@@ -86,7 +81,6 @@ import SyntaxNode from './model/SyntaxNode';
 import Token from './model/Token';
 import Transaction from './model/Transaction';
 import TransactionResult from './model/TransactionResult';
-import UndefinedError from './model/UndefinedError';
 import UpdateAction from './model/UpdateAction';
 import UpdateActionResult from './model/UpdateActionResult';
 import WorkspaceLoadProblem from './model/WorkspaceLoadProblem';
@@ -164,12 +158,6 @@ export {
     Area,
 
     /**
-     * The ArityMismatchError model constructor.
-     * @property {module:model/ArityMismatchError}
-     */
-    ArityMismatchError,
-
-    /**
      * The AzureIntegration model constructor.
      * @property {module:model/AzureIntegration}
      */
@@ -218,12 +206,6 @@ export {
     CollectProblemsActionResult,
 
     /**
-     * The ComparisonChainError model constructor.
-     * @property {module:model/ComparisonChainError}
-     */
-    ComparisonChainError,
-
-    /**
      * The Cons model constructor.
      * @property {module:model/Cons}
      */
@@ -254,12 +236,6 @@ export {
     FileSyntax,
 
     /**
-     * The FrontProblem model constructor.
-     * @property {module:model/FrontProblem}
-     */
-    FrontProblem,
-
-    /**
      * The ICViolation model constructor.
      * @property {module:model/ICViolation}
      */
@@ -282,12 +258,6 @@ export {
      * @property {module:model/InfraError}
      */
     InfraError,
-
-    /**
-     * The InlineInconsistent model constructor.
-     * @property {module:model/InlineInconsistent}
-     */
-    InlineInconsistent,
 
     /**
      * The InstallAction model constructor.
@@ -506,12 +476,6 @@ export {
     SetOptionsActionResult,
 
     /**
-     * The ShadowError model constructor.
-     * @property {module:model/ShadowError}
-     */
-    ShadowError,
-
-    /**
      * The Source model constructor.
      * @property {module:model/Source}
      */
@@ -564,12 +528,6 @@ export {
      * @property {module:model/TransactionResult}
      */
     TransactionResult,
-
-    /**
-     * The UndefinedError model constructor.
-     * @property {module:model/UndefinedError}
-     */
-    UndefinedError,
 
     /**
      * The UpdateAction model constructor.
