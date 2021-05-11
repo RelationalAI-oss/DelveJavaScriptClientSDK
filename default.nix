c4ea73c6a16eb1996c48b686d318ded9b8b7100f
Rob Bearman <robbear@hyperfine.com>
2021-05-11 09:30:22 -0700

    Unify RAICloud APIs with Rel Database APIs in JavaScript SDK (#4529)
    Co-authored-by: Nathan Daly <44379820+rai-nhdaly@users.noreply.github.com>

--- default.nix
+++ default.nix
@@ -28,24 +28,14 @@ let
 
     inherit doCheck;
   });
-
-  cloud_client = (import ./cloud_client { inherit pkgs; }).package.overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs ++ [ delveBinary ];
-    checkPhase = ''
-      echo "No current checks for cloud client..."
-    '';
-
-    inherit doCheck;
-  });
 in
 stdenv.mkDerivation rec {
   name = "delve-javascript-client-sdk-${version}";
   version = "1.1.3";
   src = ./.;
   installPhase = ''
-    mkdir -p $out/{client,cloud_client,docs}
+    mkdir -p $out/{client,docs}
     cp -rv ${client}/* $out/client
-    cp -rv ${cloud_client}/* $out/cloud_client
     cp -rv docs/* $out/docs
   '';
 }
