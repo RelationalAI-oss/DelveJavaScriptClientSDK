cef8a9ac1806e616e0a8a28b343d73ffaea5b14b
Rob Bearman <robbear@hyperfine.com>
2021-05-26 11:13:01 -0700

    Updates to the JavaScript SDK and test environment (#4642)
    * Added updateDatabase back to RAICloudAPIMixin. Update RAICloudAPIMixin unit tests. Updates to test harness app.
    * Add path to installSource. Fixes #4624
    * Remove computeName and computeRegion from Connection object and make them RelAPI parameters
    * Update RAICloudAPI tests with updated Connection object
    * Update TestHarness with new Connection object
    * Hardcode region to us-east in DefaultApi.js
    * Update patches
    * Update test harness README
    * Remove deprecated dryrun parameter

--- harness/client/components/TestHarness.js
+++ harness/client/components/TestHarness.js
@@ -5,16 +5,29 @@ class TestHarness extends LitElement {
 
   static get styles() {
     return css`
-      #listComputesTxt {
-        width: 150px;
-        height: 150px;
+      .test-section {
+        margin-bottom: 2em;
+      }
+      .test-section-title {
+        font-size: 1.2em;
+        font-weight: 700;
+        margin-bottom: 0.6em;
+      }
+      .text-list-explorer {
+        margin-left: 10px;
+        font-family: monospace
+      }
+      .list-explorer {
+        display: none;
+        margin-bottom: 0.8em;
       }
     `;
   }
 
   static get properties() {
     return {
-      computesList: { type: Array }
+      computesList: { type: Array },
+      databasesList: { type: Array }
     }
   }
 
@@ -22,6 +35,7 @@ class TestHarness extends LitElement {
     super();
 
     this.computesList = [];
+    this.databasesList = [];
 
     this._accessToken = window.testharness.config.testAccessToken;
     this._raiConnection = new Connection({
@@ -34,14 +48,17 @@ class TestHarness extends LitElement {
     const root = super.createRenderRoot();
 
     root.addEventListener('click', (ev) => {
-      const computeName = root.getElementById('computeNameInput').value;
+      let computeName;
+      let dbname;
 
       switch (ev.target.id) {
         case 'createComputeBtn':
+          computeName = root.getElementById('computeNameInput').value;
           this._createCompute(computeName);
           break;
 
         case 'deleteComputeBtn':
+          computeName = root.getElementById('computeNameInput').value;
           this._deleteCompute(computeName);
           break;
 
@@ -49,9 +66,34 @@ class TestHarness extends LitElement {
           this._listComputes();
           break;
 
+        case 'updDatabasesBtn':
+          this._listDatabases();
+          break;
+
         case 'connectDBBtn':
-          const connectDatabaseName = root.getElementById('connectDBInput').value;
-          this._connectToDatabase(connectDatabaseName);
+          dbname = root.getElementById('connectDBInput').value;
+          this._connectToDatabase(dbname);
+          break;
+
+        case 'createDBBtn':
+          dbname = root.getElementById('createDBInput').value;
+          computeName = root.getElementById('createDBComputeInput').value;
+          computeName = computeName ?? (computeName.length > 0 ? computeName : null);
+          this._createDatabase(dbname, computeName, true);
+          break;
+
+        case 'runQueryBtn':
+          dbname = root.getElementById('queryDBInput').value;
+          computeName = root.getElementById('queryComputeInput').value;
+          const query = root.getElementById('queryInput').value;
+          this._runQuery(dbname, computeName, query);
+          break;
+
+        case 'updateDBBtn':
+        case 'updateDBAndRemoveBtn':
+          dbname = root.getElementById('updateDBInput').value;
+          computeName = root.getElementById('updateDBComputeInput').value;
+          this._updateDatabase(dbname, computeName, ev.target.id === 'updateDBAndRemoveBtn');
           break;
 
         default:
@@ -64,6 +106,8 @@ class TestHarness extends LitElement {
 
   firstUpdated() {
     // Called by Lit on the first render
+    this._listComputes();
+    this._listDatabases();
 
     super.firstUpdated();
   }
@@ -77,43 +121,154 @@ class TestHarness extends LitElement {
   updated(changed) {
     // Called by Lit at the end of the render cycle
 
+    if (changed.has('computesList')) {
+      const computeItems = this.computesList.map(item => {
+        return `
+          <div class="text-list-explorer" aria-label="${item.name}">
+            <div>account_name: ${item.account_name ?? ''}</div>
+            <div>created_by: ${item.created_by ?? ''}</div>
+            <div>created_on: ${item.created_on ?? ''}</div>
+            <div>deleted_on: ${item.deleted_on ?? ''}</div>
+            <div>id: ${item.id ?? ''}</div>
+            <div>infrastructure: ${item.infrastructure ?? ''}</div>
+            <div>message: ${item.message ?? ''}</div>
+            <div>name: ${item.name ?? ''}</div>
+            <div>region: ${item.region ?? ''}</div>
+            <div>requested_on: ${item.requested_on ?? ''}</div>
+            <div>size: ${item.size ?? ''}</div>
+            <div>state: ${item.state ?? ''}</div>
+          </div>
+        `;
+      });
+
+      const elem = this.shadowRoot.getElementById('computesList');
+      if (computeItems.length > 0) {
+        const html = computeItems.join('');
+        elem.innerHTML = html;
+        elem.style.display = 'block';
+      }
+      else {
+        elem.style.display = 'none';
+      }
+    }
+
+    if (changed.has('databasesList')) {
+      const databaseItems = this.databasesList.map(item => {
+        return `
+          <div class="text-list-explorer" aria-label="${item.name}">
+            <div>account_name: ${item.account_name ?? ''}</div>
+            <div>created_by: ${item.created_by ?? ''}</div>
+            <div>default_compute_name: ${item.default_compute_name ?? ''}</div>
+            <div>id: ${item.id ?? ''}</div>
+            <div>name: ${item.name ?? ''}</div>
+            <div>region: ${item.region ?? ''}</div>
+            <div>state: ${item.state ?? ''}</div>
+          </div>
+        `;
+      });
+
+      const elem = this.shadowRoot.getElementById('databasesList');
+      if (databaseItems.length > 0) {
+        const html = databaseItems.join('');
+        elem.innerHTML = html;
+        elem.style.display = 'block';
+      }
+      else {
+        elem.style.display = 'none';
+      }
+    }
+
     super.updated(changed);
   }
 
   render() {
-    const computesOutput = JSON.stringify(this.computesList);
 
     return html`
-      <h1>Test Harness</h1>
+      <h1>RAICloud API Tests</h1>
       <div class="test-section">
+        <div class="test-section-title">Create/delete compute</div>
         <input type="text" id="computeNameInput" placeholder="Name of compute" />
         <button id="createComputeBtn">Create compute</button>
         <button id="deleteComputeBtn">Delete compute</button>
       </div>
       <div class="test-section">
-        <textarea id="listComputesTxt" placeholder="List of computes" value=${computesOutput}></textarea>
-        <button id="updComputesBtn">Update computes list</button>
+        <div class="test-section-title">List of computes</div>
+        <elix-list-explorer class="list-explorer" id="computesList"></elix-list-explorer>
+        <button id="updComputesBtn">Refresh computes list</button>
       </div>
       <div class="test-section">
+        <div class="test-section-title">List of databases</div>
+        <elix-list-explorer class="list-explorer" id="databasesList"></elix-list-explorer>
+        <button id="updDatabasesBtn">Refresh databases list</button>
+      </div>
+      <div class="test-section">
+        <div class="test-section-title">Ping test</div>
         <input type="text" id="connectDBInput" placeholder="Database name" />
         <button id="connectDBBtn">Connect to database</button>
+      </div>
+      <div class="test-section">
+        <div class="test-section-title">Create database</div>
+        <input type="text" id="createDBInput" placeholder="Database name" />
+        <input type="text" id="createDBComputeInput" placeholder="Compute name" />
+        <button id="createDBBtn">Create new database</button>
+      </div>
+      <div class="test-section">
+        <div class="test-section-title">Query</div>
+        <input type="text" id="queryInput" placeholder="Query" />
+        <input type="text" id="queryDBInput" placeholder="Database name" />
+        <input type="text" id="queryComputeInput" placeholder="Compute name" />
+        <button id="runQueryBtn">Run query</button>
+      </div>
+      <div>
+        <div class="test-section-title">Update database</div>
+        <input type="text" id="updateDBInput" placeholder="Database name" />
+        <input type="text" id="updateDBComputeInput" placeholder="Default compute name" />
+        <button id="updateDBBtn">Update database</button>
+        <button id="updateDBAndRemoveBtn">Update and remove default</button>
       </div>
     `;
   }
 
-  _createCompute(name) {
-    console.log(`Attempt to create compute ${name}`);
+  async _createCompute(name) {
+    try {
+      const response = await this._raiConnection.createCompute(name);
+      console.log(`Create compute ${name} : `, response);
+
+      if (response.result) {
+        this._listComputes();
+      }
+    }
+    catch(error) {
+      console.error(error);
+    }
   }
 
-  _deleteCompute(name) {
-    console.log(`Attempt to delete compute ${name}`);
+  async _deleteCompute(name) {
+    if (!name || name === '') {
+      return;
+    }
+
+    if (!confirm(`Are you sure you want to delete the compute named "${name}"?`)) {
+      return;
+    }
+
+    try {
+      const response = await this._raiConnection.deleteCompute(name);
+      console.log(`Delete compute: ${name}`, response);
+
+      if (response.result) {
+        this._listComputes();
+      }
+    }
+    catch(error) {
+      console.error(error);
+    }
   }
 
   async _listComputes() {
     try {
       const response = await this._raiConnection.listComputes();
-      console.log(response.result);
-      console.log(response.error);
+      console.log(`List computes: `, response);
 
       if (response.result) {
         this.computesList = response.result.computes;
@@ -124,11 +279,63 @@ class TestHarness extends LitElement {
     }
   }
 
+  async _listDatabases() {
+    try {
+      const response = await this._raiConnection.listDatabases();
+      console.log(`List databases: `, response);
+
+      if (response.result) {
+        this.databasesList = response.result.databases;
+      }
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
+
   async _connectToDatabase(dbname) {
     try {
-      const response = await this._raiConnection.connectToDatabase(dbname);
-      console.log(response.result);
-      console.log(response.error);
+      const response = await this._raiConnection.connectToDatabase(dbname, null);
+      console.log('Connect to database: ', response);
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
+
+  async _createDatabase(dbname, compute, overwrite) {
+    try {
+      const response = await this._raiConnection.createDatabase(dbname, compute, overwrite);
+      console.log('Create database: ', response);
+
+      if (response.result) {
+        this._listDatabases();
+      }
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
+
+  async _runQuery(dbname, computeName, query) {
+    try {
+      const response = await this._raiConnection.query(dbname, computeName, query);
+      console.log('Query: ', response);
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
+
+  async _updateDatabase(dbname, defaultComputeName, removeDefaultCompute) {
+    try {
+      const response = await this._raiConnection.updateDatabase(dbname, defaultComputeName, removeDefaultCompute);
+      console.log('Update database: ', response);
+
+      if (response.result) {
+        this._listDatabases();
+        this._listComputes();
+      }
     }
     catch(error) {
       console.error(error);
